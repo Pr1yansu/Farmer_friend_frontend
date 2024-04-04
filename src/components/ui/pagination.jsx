bee010a9bdd3c39e198ed82b7723191d9e43e6b6
purbarunBC13 <purbarunmondal@gmail.com>
2024-04-04 19:47:17 +0530

    history created

---
+++ src/components/ui/pagination.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactPaginate from 'react-paginate';
+
+const Pagination = ({ itemsPerPage, items }) => {
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+  return (
+    <>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+};
+
+export default Pagination;
