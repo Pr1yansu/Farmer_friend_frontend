aa8637cef970467ae8aab372af2e619e17fbb1e3
Pr1yansu <113017699+Pr1yansu@users.noreply.github.com>
2024-04-04 19:50:01 +0530

    Image upload error loading animation

--- src/components/ui/loader.jsx
+++ src/components/ui/loader.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Lottie from "lottie-react";
+import TreeAnimation from "../../lotties/Tree-loading-animation.json";
+import styles from "../styles/loader.module.css";
 
 const Loader = () => {
-  return <div>hi</div>;
+  return (
+    <div className={styles.loader}>
+      <Lottie animationData={TreeAnimation} />
+    </div>
+  );
 };
 
 export default Loader;
--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -2,21 +2,24 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import styles from "../styles/history.module.css";
+import Loader from "../components/ui/loader";
 const History = () => {
   const [loading, setLoading] = useState(true);
-  const [history,setHistory] = useState();
+  const [history, setHistory] = useState();
   useEffect(() => {
     const url = process.env.REACT_APP_SERVER_URL;
-    const fecthHistory = async ()=>{
+    const fecthHistory = async () => {
       try {
-        const {data} = await axios.post(`${url}/api/history/view`,{
-
-        }, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
+        const { data } = await axios.post(
+          `${url}/api/history/view`,
+          {},
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
         console.log(data);
         if (data != null) {
           setHistory(data);
@@ -30,31 +33,29 @@ const History = () => {
     fecthHistory();
   }, []);
 
-
   return (
     <>
       {loading ? (
-        <div className={styles.test}>Loading...</div>
+        <Loader />
       ) : (
         <main className={styles.container}>
           <h1>History</h1>
           <div className={styles.headings}>
-            <h2 style={{width: "80px"}}>Name</h2>
-            <h2 style={{width: "270px"}}>Date</h2>
-            <h2 style={{width: "450px"}}>Description</h2>
+            <h2 style={{ width: "80px" }}>Name</h2>
+            <h2 style={{ width: "270px" }}>Date</h2>
+            <h2 style={{ width: "450px" }}>Description</h2>
           </div>
           <div className={styles.datas}>
-              {history.map((item, index) => (
-                <div key={index} className={styles.rows}>
-                  <p>{item.name}</p>
-                  <p>{item.date}</p>
-                  <p>{item.desc}</p>
-                </div>
-              ))}
+            {history.map((item, index) => (
+              <div key={index} className={styles.rows}>
+                <p>{item.name}</p>
+                <p>{item.date}</p>
+                <p>{item.desc}</p>
+              </div>
+            ))}
           </div>
         </main>
       )}
-      {/* <h1 className="test">HI</h1> */}
     </>
   );
 };
